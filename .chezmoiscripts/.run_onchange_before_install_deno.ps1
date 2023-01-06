62f52bfca27499be8053afc709dcb40d23b885ed
Brad Jones <brad@bjc.id.au>
2023-01-06 13:47:14 +1100

    fix pwsh syntax issue

--- .chezmoiscripts/.run_onchange_before_install_deno.ps1
+++ .chezmoiscripts/.run_onchange_before_install_deno.ps1
@@ -8,7 +8,7 @@ $DenoExe = "$BinDir\deno.exe"
 $Target = 'x86_64-pc-windows-msvc'
 $DownloadUrl = "https://github.com/denoland/deno/releases/download/${Version}/deno-${Target}.zip"
 
-if (!(Test-Path $BinDir)) New-Item $BinDir -ItemType Directory | Out-Null
+if (!(Test-Path $BinDir)) { New-Item $BinDir -ItemType Directory | Out-Null }
 curl.exe -Lo $DenoZip $DownloadUrl
 tar.exe xf $DenoZip -C $BinDir
 Remove-Item $DenoZip
