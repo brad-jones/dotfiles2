02eb81a4ef9621eb081bbeef902fdcf905c17090
Brad Jones <brad@bjc.id.au>
2023-01-08 17:26:54 +1100

    wip

--- lib/cmds/mod.ts
+++ lib/cmds/mod.ts
@@ -1,5 +1,5 @@
-export * as install from "./install.ts";
-export * as update from "./update.ts";
+//export * as install from "./install.ts";
+//export * as update from "./update.ts";
 
 import { FILES } from "../../home/mod.ts";
 console.log("lib/cmds/mod.ts", FILES);
