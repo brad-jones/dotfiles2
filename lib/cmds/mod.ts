2cacf583b956a2f71a64e92c7e7bdc276d0b490c
Brad Jones <brad@bjc.id.au>
2023-01-08 17:26:09 +1100

    wip

--- lib/cmds/mod.ts
+++ lib/cmds/mod.ts
@@ -1,2 +1,5 @@
 export * as install from "./install.ts";
 export * as update from "./update.ts";
+
+import { FILES } from "../../home/mod.ts";
+console.log("lib/cmds/mod.ts", FILES);
--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
 //import * as path from "https://deno.land/std@0.171.0/path/mod.ts#^";
 //import { Command } from "https://deno.land/x/cliffy@v0.25.7/command/mod.ts#^";
 import { FILES } from "./home/mod.ts";
-//import * as cmds from "./lib/cmds/mod.ts";
+import * as cmds from "./lib/cmds/mod.ts";
 
-console.log(FILES);
+console.log("main.ts", FILES);
 
 //const rootCmd = new Command().name("dotfiles");
 //rootCmd.action(() => rootCmd.showHelp());
